2ebcfcf2aa2127dae854b50edfffc4854fa5ce56
Antonio <tonihm96@hotmail.com>
2023-02-02 22:55:00 -0300

    New screens & code refactoring

--- src/routes/stack.routes.tsx
+++ src/routes/stack.routes.tsx
@@ -1,9 +1,15 @@
 import React from 'react';
+import { RouteProp } from '@react-navigation/native';
 import {
   createNativeStackNavigator,
   NativeStackNavigationOptions
 } from '@react-navigation/native-stack';
 
+import { NavigationStackParamList } from './Models';
+
+import { SignIn } from '../screens/SignIn';
+import { SignUpFirstStep } from '../screens/SignUp/SignUpFirstStep';
+import { SignUpSecondStep } from '../screens/SignUp/SignUpSecondStep';
 import { Splash } from '../screens/Splash';
 import { Home } from '../screens/Home';
 import { CarDetails } from '../screens/CarDetails';
@@ -12,24 +18,26 @@ import { SchedulingDetails } from '../screens/SchedulingDetails';
 import { SchedulingComplete } from '../screens/SchedulingComplete';
 import { MyCars } from '../screens/MyCars';
 
-import { NavigationStackProps } from './Models';
-
 const { Navigator, Screen } =
-  createNativeStackNavigator<NavigationStackProps>();
+  createNativeStackNavigator<NavigationStackParamList>();
 
-const screenOptions: NativeStackNavigationOptions = {
-  headerShown: false
-};
+const screenOptions = ({
+  route
+}: {
+  route: RouteProp<NavigationStackParamList, keyof NavigationStackParamList>;
+}): NativeStackNavigationOptions => ({
+  headerShown: false,
+  gestureEnabled: !(route.name === 'Home')
+});
 
 export function StackRoutes() {
   return (
     <Navigator screenOptions={screenOptions} initialRouteName='Splash'>
       <Screen name='Splash' component={Splash} />
-      <Screen
-        name='Home'
-        component={Home}
-        options={{ gestureEnabled: false }}
-      />
+      <Screen name='SignIn' component={SignIn} />
+      <Screen name='SignUpFirstStep' component={SignUpFirstStep} />
+      <Screen name='SignUpSecondStep' component={SignUpSecondStep} />
+      <Screen name='Home' component={Home} />
       <Screen name='CarDetails' component={CarDetails} />
       <Screen name='Scheduling' component={Scheduling} />
       <Screen name='SchedulingDetails' component={SchedulingDetails} />
